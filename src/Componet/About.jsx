7c58f5aa3d6ef48b5634cf1e5534a6ea81b0d2b5
tushar <tushargadher123@gmail.com>
2025-03-31 17:03:42 +0530

    iamge issue fixed

--- src/Componet/About.jsx
+++ src/Componet/About.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
+// Import profile image
+import profileImg from "../Media/Profile.webp";
+
+// Import skill images
+import htmlImg from "../SkillsImg/html.png";
+import cssImg from "../SkillsImg/css.png";
+import jsImg from "../SkillsImg/javascript.png";
+import reactImg from "../SkillsImg/react.png";
+import vueImg from "../SkillsImg/vue.png";
+import nuxtImg from "../SkillsImg/nuxt.png";
+import nodeImg from "../SkillsImg/nodejs.png";
+import mongoImg from "../SkillsImg/mongo.png";
+import bootstrapImg from "../SkillsImg/bootstrap.png";
+import tailwindImg from "../SkillsImg/taliwindcss.png";
+import jqueryImg from "../SkillsImg/jqurey.png";
+import mysqlImg from "../SkillsImg/mysql.png";
+import gitImg from "../SkillsImg/git.png";
+import npmImg from "../SkillsImg/npm.png";
+import netlifyImg from "../SkillsImg/netlify.png";
+import postmanImg from "../SkillsImg/postman.png";
+import materialUiImg from "../SkillsImg/matrialUi.png";
+
 function About() {
   return (
     <>
@@ -9,9 +31,11 @@ function About() {
           <h4>ABOUT</h4>
           <h1 className="introHead">KNOW MORE ABOUT ME</h1>
           <div className="mydetail">
-            <img src="src\Media\Profile.webp" alt="Profile not available" loading="lazy" />
+            <img src={profileImg} alt="Profile not available" loading="lazy" />
             <div className="detail">
-              <h1 style={{ color: "#0065FF" }}>Software Engineer | Full Stack Developer</h1>
+              <h1 style={{ color: "#0065FF" }}>
+                Software Engineer | Full Stack Developer
+              </h1>
               <p id="p">
                 Hello! I'm Tushar Gadher, a results-driven Software Engineer specializing in Vue.js, Nuxt.js, JavaScript, and GraphQL.
                 I have experience developing scalable, high-performance web applications and optimizing frontend architecture.
@@ -28,55 +52,55 @@ function About() {
         <div className="skillcontainer" data-aos="fade-up">
           <h1 className="HeadSkill">SKILLS & TOOLS</h1>
           <div className="skill" data-aos="zoom-in-up" data-title="HTML">
-            <img src="src\SkillsImg\html.png" alt="html" loading="lazy"/>
+            <img src={htmlImg} alt="HTML" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="CSS">
-            <img src="src\SkillsImg\css.png" alt="css" loading="lazy"/>
+            <img src={cssImg} alt="CSS" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="JavaScript">
-            <img src="src\SkillsImg\javascript.png" alt="javascript" loading="lazy"/>
+            <img src={jsImg} alt="JavaScript" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="ReactJs">
-            <img src="src\SkillsImg\react.png" alt="react" loading="lazy"/>
+            <img src={reactImg} alt="React" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="VueJS">
-            <img src="src\SkillsImg\vue.png" alt="vuejs" loading="lazy"/>
+            <img src={vueImg} alt="VueJS" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="NuxtJs">
-            <img src="src\SkillsImg\nuxt.png" alt="nuxtjs" loading="lazy"/>
+            <img src={nuxtImg} alt="NuxtJS" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="NodeJs">
-            <img src="src\SkillsImg\nodejs.png" alt="node" loading="lazy"/>
+            <img src={nodeImg} alt="NodeJS" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="MongoDB">
-            <img src="src\SkillsImg\mongo.png" alt="mongodb" loading="lazy"/>
+            <img src={mongoImg} alt="MongoDB" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="Bootstrap">
-            <img src="src\SkillsImg\bootstrap.png" alt="bootstrap" loading="lazy"/>
+            <img src={bootstrapImg} alt="Bootstrap" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="Tailwind CSS">
-            <img src="src\SkillsImg\taliwindcss.png" alt="tailwind" loading="lazy"/>
+            <img src={tailwindImg} alt="Tailwind CSS" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="JQuery">
-            <img src="src\SkillsImg\jqurey.png" alt="jquery" loading="lazy"/>
+            <img src={jqueryImg} alt="JQuery" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="MySQL">
-            <img src="src\SkillsImg\mysql.png" alt="mysql" loading="lazy"/>
+            <img src={mysqlImg} alt="MySQL" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="Git">
-            <img src="src\SkillsImg\git.png" alt="git" loading="lazy"/>
+            <img src={gitImg} alt="Git" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="NPM">
-            <img src="src\SkillsImg\npm.png" alt="npm" loading="lazy"/>
+            <img src={npmImg} alt="NPM" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="Netlify">
-            <img src="src\SkillsImg\netlify.png" alt="netlify" loading="lazy"/>
+            <img src={netlifyImg} alt="Netlify" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="PostMan">
-            <img src="src\SkillsImg\postman.png" alt="postman" loading="lazy"/>
+            <img src={postmanImg} alt="Postman" loading="lazy" />
           </div>
           <div className="skill" data-aos="zoom-in-up" data-title="Material UI">
-            <img src="src\SkillsImg\matrialUi.png" alt="material-ui" loading="lazy"/>
+            <img src={materialUiImg} alt="Material UI" loading="lazy" />
           </div>
         </div>
       </div>
